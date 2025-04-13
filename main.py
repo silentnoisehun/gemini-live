2b76ae29cc0963d0d049014163b8c543bdd10dde
jsalsman <jsalsman@gmail.com>
2025-04-13 03:06:11 +0000

    downscale images to save memory/tokens; format welcome message

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+#import os
 from flask import Flask, send_file, request, redirect, Response
 
 app = Flask(__name__)
@@ -12,59 +12,59 @@ def index():
         else:
             # HTML for form
             html_content = """<!DOCTYPE html>
-<html>
-<head>
+<html><head>
     <meta charset="UTF-8">
     <title>Gemini Live Voice to Text Realtime: Enter API Key</title>
     <!-- By Jim Salsman, April 2025. Released under the free MIT License. -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
         body {
-            background-color: lightgreen;
-            font-family: sans-serif;
-            text-align: center;
+          background-color: lightgreen;
+          font-family: sans-serif;
+          text-align: center;
+        }
+        a { text-decoration: none; }
+        .description, .privacy {
+          width: 80%;
+          margin: 0 auto;
+          text-align: left;
         }
     </style>
-</head>
-<body>
+</head><body>
     <h2>Gemini Live Voice to Text Realtime</h2>
     <p>Enter your (free!) Gemini API key from:
     <a href="https://aistudio.google.com/apikey" target="_blank"
         style="text-decoration: none;">aistudio.google.com/apikey</a></p>
     <form method="POST">
-        <input type="text" size="33" name="api_key" required>
-        <button type="submit">Submit</button>
-    </form>
-    <p style="text-align: left;">
+      <input type="text" size="33" name="api_key" required>
+      <button type="submit">Submit</button>
+    </form><br>
+    <p class="description">
         This is a web application that allows you to interact with 
         Google's Gemini 2.0 Flash Live large language model using your
         voice in real-time. It's based on Google's js-genai API to run
         entirely in the browser, providing a seamless, voice-driven
         experience. Key features include Google Search, Python code
-        execution, with output rendered in markdown and LaTeX. The
-        <a href="https://github.com/jsalsman/gemini-live" target="_blank"
-            style="text-decoration: none;">source code is free on 
-            GitHub</a> under the MIT License. Voice to text
-        interactions are advantageous because most people read
-        about twice as fast as synthetic speech typically talks;
-        while text can be skimmed, whereas interrupting speech
-        leaves what might have been said a complete mystery.
-    </p>
-    <p>
-        <small>
-            <b>Privacy policy:</b> Your API key is stored in a cookie which
-            is only accessible from the HTML JavaScript at this domain, which
-            runs only in your browser. It will never be stored anywhere else.
-            If you don't trust this, fork the code on GitHub to run
-            it on your own server.
-            <br><br>
-            By <a href="mailto:jim@talknicer.com"
-            style="text-decoration: none;">Jim Salsman</a>, April 11, 2025.
-        </small>
+        execution (including image display from e.g. matplotlib),
+        image upload (including from the camera on mobile), and text
+        output rendered in markdown and LaTeX. The <a
+        href="https://github.com/jsalsman/gemini-live" target="_blank"
+        >source code is free on GitHub</a> under the MIT License.
+        Voice to text interactions are advantageous because most people
+        read about twice as fast as synthetic speech typically talks;
+        while text can be skimmed, interrupting speech leaves what
+        might have been said as an often frustrating mystery.
+    </p><br>
+    <p class="privacy">
+        <b>Privacy policy:</b> Your API key is stored in a cookie which
+        is only accessible from the HTML JavaScript at this domain, which
+        runs only in your browser. It will never be stored anywhere else.
+        If you don't trust this, fork the code on GitHub to run it on
+        your own server. By <a href="mailto:jim@talknicer.com"
+        >Jim Salsman</a>, April 11, 2025.
     </p>
 </body>
-</html>
-"""
+</html>"""
             return html_content
     elif request.method == 'POST':
         api_key = request.form.get('api_key')
@@ -81,5 +81,4 @@ def favicon():
 def robots():
     return Response("User-agent: *\nAllow: /\n", mimetype='text/plain')
 
-def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+#app.run(port=int(os.environ.get('PORT', 80)), debug=...)
